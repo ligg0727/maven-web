0208c1c5af06c761ca05b3590ca58c8189e649b3
ligg <742751377@qq.com>
2019-02-18 11:51:45 +0800

    分支提交

--- src/main/java/com/zrgj/entity/Word.java
+++ src/main/java/com/zrgj/entity/Word.java
@@ -7,6 +7,9 @@ public class Word {
     public static void main(String[] args) {
         System.out.println("dsada");
         System.out.println("asda");
+        System.out.println("asda");
+        System.out.println("asda");
+
     }
 
 }
