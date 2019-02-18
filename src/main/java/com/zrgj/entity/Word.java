160291a1bf414a121cf98be91a0b627e81ce8eba
ligg <742751377@qq.com>
2019-02-18 11:39:00 +0800

    分支提交

--- src/main/java/com/zrgj/entity/Word.java
+++ src/main/java/com/zrgj/entity/Word.java
@@ -4,4 +4,9 @@ package com.zrgj.entity;
  * Created by Administrator on 2019/2/16 0016.
  */
 public class Word {
+    public static void main(String[] args) {
+        System.out.println("dsada");
+        System.out.println("asda");
+    }
+
 }
